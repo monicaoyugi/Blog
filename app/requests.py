022922f32ada3682eda7b2fc6800b244dd893d7b
monicaoyugi <monicaoyugi@gmail.com>
2020-02-20 02:13:29 +0300

    obtained quotes from url

---
+++ app/requests.py
@@ -0,0 +1,17 @@
+import urllib.request, json
+
+base_url = None
+
+def configure_request(app):
+    global base_url
+    base_url = app.config['QUOTE_BASE_URL']
+
+def get_random_quote():
+    """Obtains the quotes from the url"""
+
+    with urllib.request.urlopen(base_url) as url:
+        quote_data = url.read()
+        quote_response = json.loads(quote_data)
+        print(quote_data)
+        
+    return quote_response
